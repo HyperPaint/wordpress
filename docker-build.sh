11855cb9f5d3dd2d5a1d4c5c167cc7b30d28f91f
HyperPaint <dioman172@yandex.ru>
2024-01-24 13:30:24 +1000

    up

--- docker-build.sh
+++ docker-build.sh
@@ -2,8 +2,8 @@
 
 repository=hyperpaint
 name=wordpress
-version=6.2
-build=3
+version=6.4.2
+build=1
 
 docker build -t $repository/$name:$version .
 docker tag $repository/$name:$version $repository/$name:$version-$build
